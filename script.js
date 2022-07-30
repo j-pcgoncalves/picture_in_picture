5ab652fea78df1f771ed99c41b41e5a26755dcbc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-07-30 20:32:13 +0100

    creating selectMediaStream function

---
+++ script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+// On Load
+selectMediaStream();
